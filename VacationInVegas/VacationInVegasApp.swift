533b401b4eb91c9a47effd8562cca5f8b5ffd6ad
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-25 21:34:28 +0530

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Mayur Vaity on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
